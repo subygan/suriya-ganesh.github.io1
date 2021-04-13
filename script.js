f4d52f89a672e85f207a49b2c6cac7100487c242
Suriya Ganesh <suriyaganesh097@gmail.com>
2021-04-13 10:24:57 +0530

    change

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const peer = new Peer(''+Math.floor(Math.random()*2**18).toString(36).padStart(4,0), {
-    host: location.hostname,
+    host: "http://suriyaganesh.com",
     debug: 1,
     path: '/myapp'
 });
