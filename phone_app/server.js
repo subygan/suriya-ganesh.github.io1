8f18c0ff20306d9f556a980be1840c55579e5f6d
Suriya Ganesh <suriyaganesh097@gmail.com>
2021-04-13 19:18:04 +0530

    cahnge

--- phone_app/server.js
+++ phone_app/server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const http = require('http');
+var https = require('https');
+var fs = require('fs');
+
 const path = require('path');
 const app = express();
-const server = http.createServer(app);
 const { ExpressPeerServer } = require('peer');
+
 const port = process.env.PORT || "80";
+var privateKey  = fs.readFileSync('./sslcert/server.key', 'utf8');
+var certificate = fs.readFileSync('./sslcert/server.crt', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
+
+const server = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
 
 const peerServer = ExpressPeerServer(server, {
     proxied: true,
@@ -22,6 +32,9 @@ app.get("/", (request, response) => {
 });
 
 server.listen(port);
+httpsServer.listen(8001, function(){
+    console.log("server running at https://IP_ADDRESS:8001/")
+});
 console.log('Listening on: ' + port);
 
 
